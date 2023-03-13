26a4e38b31b1b9002b2d9fe9dea5eaaff270c4a1
melo <qonkgonk@gmail.com>
2023-03-13 16:51:59 +0800

    modal select element

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -10,30 +10,14 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 })
 
 export class ModalComponent implements OnInit {
-  modalForm: FormGroup;
   http: HttpClient;
   iframeUrl: SafeResourceUrl;
   @Input() mainToModal: any;
+  modalCoordinate = '22.286083,114.191790';
 
-  constructor(public sanitizer: DomSanitizer, fb: FormBuilder, http: HttpClient) {
-    this.modalForm = fb.group({
-      'modalMarketID': null,
-      'modalName_e': null,
-      'modalName_c': null,
-      'modalRegion_e': null,
-      'modalRegion_c': null,
-      'modalDistrict_e': null,
-      'modalDistrict_c': null,
-      'modalAddress_e': null,
-      'modalAddress_c': null,
-      'modalBH_e': null,
-      'modalBH_c': null,
-      'modalContact1': null,
-      'modalContact2': null,
-      'modalCoordinate': null
-    });
+  constructor(public sanitizer: DomSanitizer, http: HttpClient) {
     this.http = http;
-    this.iframeUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://maps.google.com/maps?q=' + this.modalForm.value.modalCoordinate + '&t=&z=13&ie=UTF8&iwloc=&output=embed');
+    this.iframeUrl = this.sanitizer.bypassSecurityTrustResourceUrl('https://maps.google.com/maps?q=' + this.modalCoordinate + '&t=&z=13&ie=UTF8&iwloc=&output=embed');
   }
 
   ngOnInit() {
@@ -44,12 +28,29 @@ export class ModalComponent implements OnInit {
     console.log('this.iframeUrl: ' + this.iframeUrl);
   }
 
-  resetModalForm() {
-    this.modalForm.reset();
+  resetModalForm(modalRegion_e: HTMLSelectElement, modalCoordinate: HTMLInputElement) {
+    this.isModalEditable(this.mainToModal.action, modalRegion_e, modalCoordinate);
+    modalCoordinate.value = '';
   }
 
-  setModalForm() {
-    this.modalForm.controls['modalMarketID'].setValue(this.mainToModal.mID);
-    console.log(this.modalForm.value);
+  setModalForm(modalRegion_e: HTMLSelectElement, modalCoordinate: HTMLInputElement) {
+    this.isModalEditable(this.mainToModal.action, modalRegion_e, modalCoordinate);
+    modalCoordinate.value = this.mainToModal.mID;
+    modalRegion_e.options.remove(0);
+    const opt1 = document.createElement("option");
+    const opt2 = document.createElement("option");
+    opt1.value = 'KLN';
+    opt1.text = 'Kowloon';
+    opt2.value = 'HK';
+    opt2.text = 'Hong Kong';
+    opt2.selected = true;
+    modalRegion_e.add(opt1, null);
+    modalRegion_e.add(opt2, null);
+    console.log('setModalForm');
+  }
+
+  isModalEditable(action: string, modalRegion_e: HTMLSelectElement, modalCoordinate: HTMLInputElement) {
+    modalCoordinate.readOnly = action == 'detail' ? true : false;
+    modalRegion_e.disabled = action == 'detail' ? true : false;
   }
 }
